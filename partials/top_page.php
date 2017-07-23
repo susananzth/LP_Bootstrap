6c8ca08b3c7a4aaeebf767fb62ebc0f28d7547d7
Susana Piñero <susananzth@gmail.com>
2017-07-23 17:29:27 -0400

    Añadiendo el top de las paginas a top_pague.php

--- partials/top_page.php
+++ partials/top_page.php
@@ -1,3 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name='language' content='ES'>
+    <meta name='keywords' lang="ES" content='bootstrap, plantilla, diseño web, diseño, html, css,
+                                             Susana Piñero, susananzth'>
+    <meta name='keywords' lang="EN" content='bootstrap, template, web desing, desing, html, css,
+                                             Susana Piñero, susananzth'>
+    <meta name='description' lang="ES" content='Una Página web usando bootstrap, con landing page,
+                                                sección de artículos, galería de imágenes y vídeo,
+                                                contacto por Susana Piñero.'>
+    <meta name='description' lang="EN" content='Website using bootstrap, with landing page,
+                                                section of articles, gallery of images and video,
+                                                contact by Susana Piñero.'>
+    <meta name='author' content='Susana Piñero, susananzth@gmail.com'>
+    <meta name='target' content='all'>
+    <meta name='HandheldFriendly' content='True'>
+    <meta name='robots' content='noarchive'>
+    <meta http-equiv="title" lang="ES" content="Plantilla usando Bootstrap - por Susana Piñero"/>
+    <meta http-equiv="title" lang="EN" content="Template using Bootstrap - by Susana Piñero"/>
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Set-Cookie" content="name=data; path=path; expires=0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/mystyle.css">
+
+    <title>Template with Bootstrap</title>
+</head>
+<body>
+
 <?php
 /**
  * Created by PhpStorm.
