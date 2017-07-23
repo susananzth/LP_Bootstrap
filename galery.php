94d5915eabf56d3dcbb1737637757a90773c8b15
Susana Piñero <susananzth@gmail.com>
2017-07-23 18:37:44 -0400

    Modificando galery, añadiendo imágenes

--- galery.php
+++ galery.php
@@ -11,7 +11,7 @@
 
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+                    <img class="img-responsive" src="img/01.jpg" alt="">
                 </a>
                 <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
                 <div id="modal01" class="modal fade" role="dialog">
@@ -23,7 +23,7 @@
                                 <h4 class="modal-title text-center">Primera imágen</h4>
                             </div>
                             <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
+                                <img class="img-responsive thumbnail" src="img/01.jpg" alt="Primera imágen">
                             </div>
                             <div class="modal-footer">
                                 <p>Descripción de la primera imágen.</p>
@@ -34,22 +34,22 @@
             </div>
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+                    <img class="img-responsive" src="img/02.jpg" alt="">
                 </a>
-                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
-                <div id="modal01" class="modal fade" role="dialog">
+                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal02">Abrir en detalles</button>
+                <div id="modal02" class="modal fade" role="dialog">
                     <div class="modal-dialog">
 
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title text-center">Primera imágen</h4>
+                                <h4 class="modal-title text-center">Segunda imágen</h4>
                             </div>
                             <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
+                                <img class="img-responsive thumbnail" src="img/02.jpg" alt="Segunda imágen">
                             </div>
                             <div class="modal-footer">
-                                <p>Descripción de la primera imágen.</p>
+                                <p>Descripción de la segunda imágen.</p>
                             </div>
                         </div>
                     </div>
@@ -57,22 +57,22 @@
             </div>
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+                    <img class="img-responsive" src="img/03.jpg" alt="">
                 </a>
-                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
-                <div id="modal01" class="modal fade" role="dialog">
+                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal03">Abrir en detalles</button>
+                <div id="modal03" class="modal fade" role="dialog">
                     <div class="modal-dialog">
 
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title text-center">Primera imágen</h4>
+                                <h4 class="modal-title text-center">Tercera imágen</h4>
                             </div>
                             <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
+                                <img class="img-responsive thumbnail" src="img/03.jpg" alt="Tercera imágen">
                             </div>
                             <div class="modal-footer">
-                                <p>Descripción de la primera imágen.</p>
+                                <p>Descripción de la tercera imágen.</p>
                             </div>
                         </div>
                     </div>
@@ -80,22 +80,22 @@
             </div>
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+                    <img class="img-responsive" src="img/04.jpg" alt="">
                 </a>
-                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
-                <div id="modal01" class="modal fade" role="dialog">
+                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal04">Abrir en detalles</button>
+                <div id="modal04" class="modal fade" role="dialog">
                     <div class="modal-dialog">
 
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title text-center">Primera imágen</h4>
+                                <h4 class="modal-title text-center">Cuarta imágen</h4>
                             </div>
                             <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
+                                <img class="img-responsive thumbnail" src="img/04.jpg" alt="Cuarta imágen">
                             </div>
                             <div class="modal-footer">
-                                <p>Descripción de la primera imágen.</p>
+                                <p>Descripción de la cuarta imágen.</p>
                             </div>
                         </div>
                     </div>
@@ -103,22 +103,22 @@
             </div>
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+                    <img class="img-responsive" src="img/05.jpg" alt="">
                 </a>
-                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
-                <div id="modal01" class="modal fade" role="dialog">
+                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal05">Abrir en detalles</button>
+                <div id="modal05" class="modal fade" role="dialog">
                     <div class="modal-dialog">
 
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title text-center">Primera imágen</h4>
+                                <h4 class="modal-title text-center">Quinta imágen</h4>
                             </div>
                             <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
+                                <img class="img-responsive thumbnail" src="img/05.jpg" alt="Quinta imágen">
                             </div>
                             <div class="modal-footer">
-                                <p>Descripción de la primera imágen.</p>
+                                <p>Descripción de la quinta imágen.</p>
                             </div>
                         </div>
                     </div>
@@ -126,22 +126,22 @@
             </div>
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+                    <img class="img-responsive" src="img/06.jpg" alt="">
                 </a>
-                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
-                <div id="modal01" class="modal fade" role="dialog">
+                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal06">Abrir en detalles</button>
+                <div id="modal06" class="modal fade" role="dialog">
                     <div class="modal-dialog">
 
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title text-center">Primera imágen</h4>
+                                <h4 class="modal-title text-center">Sexta imágen</h4>
                             </div>
                             <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
+                                <img class="img-responsive thumbnail" src="img/06.jpg" alt="Sexta imágen">
                             </div>
                             <div class="modal-footer">
-                                <p>Descripción de la primera imágen.</p>
+                                <p>Descripción de la sexta imágen.</p>
                             </div>
                         </div>
                     </div>
@@ -149,22 +149,22 @@
             </div>
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+                    <img class="img-responsive" src="img/07.jpg" alt="">
                 </a>
-                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
-                <div id="modal01" class="modal fade" role="dialog">
+                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal07">Abrir en detalles</button>
+                <div id="modal07" class="modal fade" role="dialog">
                     <div class="modal-dialog">
 
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title text-center">Primera imágen</h4>
+                                <h4 class="modal-title text-center">Séptima imágen</h4>
                             </div>
                             <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
+                                <img class="img-responsive thumbnail" src="img/07.jpg" alt="Séptima imágen">
                             </div>
                             <div class="modal-footer">
-                                <p>Descripción de la primera imágen.</p>
+                                <p>Descripción de la sétima imágen.</p>
                             </div>
                         </div>
                     </div>
@@ -172,22 +172,22 @@
             </div>
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+                    <img class="img-responsive" src="img/08.jpg" alt="">
                 </a>
-                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
-                <div id="modal01" class="modal fade" role="dialog">
+                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal08">Abrir en detalles</button>
+                <div id="modal08" class="modal fade" role="dialog">
                     <div class="modal-dialog">
 
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title text-center">Primera imágen</h4>
+                                <h4 class="modal-title text-center">Octava imágen</h4>
                             </div>
                             <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
+                                <img class="img-responsive thumbnail" src="img/08.jpg" alt="Octava imágen">
                             </div>
                             <div class="modal-footer">
-                                <p>Descripción de la primera imágen.</p>
+                                <p>Descripción de la octava imágen.</p>
                             </div>
                         </div>
                     </div>
@@ -195,91 +195,22 @@
             </div>
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+                    <img class="img-responsive" src="img/09.jpg" alt="">
                 </a>
-                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
-                <div id="modal01" class="modal fade" role="dialog">
+                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal09">Abrir en detalles</button>
+                <div id="modal09" class="modal fade" role="dialog">
                     <div class="modal-dialog">
 
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title text-center">Primera imágen</h4>
+                                <h4 class="modal-title text-center">Novena imágen</h4>
                             </div>
                             <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
+                                <img class="img-responsive thumbnail" src="img/09.jpg" alt="Novena imágen">
                             </div>
                             <div class="modal-footer">
-                                <p>Descripción de la primera imágen.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
-                </a>
-                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
-                <div id="modal01" class="modal fade" role="dialog">
-                    <div class="modal-dialog">
-
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title text-center">Primera imágen</h4>
-                            </div>
-                            <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
-                            </div>
-                            <div class="modal-footer">
-                                <p>Descripción de la primera imágen.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
-                </a>
-                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
-                <div id="modal01" class="modal fade" role="dialog">
-                    <div class="modal-dialog">
-
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title text-center">Primera imágen</h4>
-                            </div>
-                            <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
-                            </div>
-                            <div class="modal-footer">
-                                <p>Descripción de la primera imágen.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#">
-                    <img class="img-responsive" src="http://placehold.it/400x300" alt="">
-                </a>
-                <button type="button" class="btn btn-default btn-block" data-toggle="modal" data-target="#modal01">Abrir en detalles</button>
-                <div id="modal01" class="modal fade" role="dialog">
-                    <div class="modal-dialog">
-
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title text-center">Primera imágen</h4>
-                            </div>
-                            <div class="modal-body">
-                                <img class="img-responsive thumbnail" src="http://placehold.it/400x300" alt="">
-                            </div>
-                            <div class="modal-footer">
-                                <p>Descripción de la primera imágen.</p>
+                                <p>Descripción de la novena imágen.</p>
                             </div>
                         </div>
                     </div>
