cb554de1fde3d0647c3027b91ef67c3962d698aa
Susana Piñero <susananzth@gmail.com>
2017-07-23 17:31:12 -0400

    Añadiendo el nav de las páginas en nav.php

--- login.php
+++ login.php
@@ -1,31 +1,4 @@
 
-<nav id="navbar" class="navbar navbar-default" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <!--Mobile menu button | Botón de menú móvil-->
-            <button type="button" class="navbar-toggle" data-toggle="collapse"
-                    data-target="#nav">
-                <span class="glyphicon glyphicon-menu-hamburger"></span>
-            </button>
-            <!--Logo-->
-            <div class="navbar-header">
-                <a class="navbar-brand" href="index.html">
-                    Susana Piñero <small> SusanaNzth</small>
-                </a>
-            </div>
-        </div>
-        <!--Drop-down navigation bar | Barra de navegación desplegable-->
-        <div class="collapse navbar-collapse" id="nav">
-            <ul class="nav navbar-nav navbar-right text-right">
-                <li><a href="index.html">Inicio</a></li>
-                <li><a href="article.html">Artículos</a></li>
-                <li><a href="galery.html">Galería</a></li>
-                <li><a href="login.php">Regístrate</a></li>
-                <li><a href="contact.html">Contáctame</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
 <section>
     <div class="container ecc">
         <div class="row">
